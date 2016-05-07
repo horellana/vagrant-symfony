0ff333c8cc56f280d8f964e62bb21e9abdc6a83a
Hector Orellana <pulga.p.29@gmail.com>
2016-05-07 05:52:40 -0300

    install git

--- vagrant.sh
+++ vagrant.sh
@@ -4,6 +4,7 @@ sudo apt-get upgrade -y --force-yes
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get install -y -f --force-yes php5 php5-mcrypt php5-cli php5-mysql mysql-server apache2 libapache2-mod-php5
+sudo apt-get install -y -f --force-yes git
 
 sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
